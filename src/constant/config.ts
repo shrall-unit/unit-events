9b611fd7eb25d7e4961cde3032b479aa3b4d43b4
shrall <shrallvierdo@gmail.com>
2023-12-21 13:07:43 +0700

    chore: setup SEO

---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+    title: 'Next.js + Tailwind CSS + TypeScript Starter',
+    description:
+        'A starter for Next.js, Tailwind CSS, and TypeScript',
+    url: 'localhost:3000',
+};
