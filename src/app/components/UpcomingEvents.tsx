97818c86cc43930299594a0fdcb61fa37e03e60d
shrall-unit <marshall.kurniawan@unitcode.org>
2024-02-28 11:01:25 +0700

    feat: add mobile navbar

--- src/app/components/UpcomingEvents.tsx
+++ src/app/components/UpcomingEvents.tsx
@@ -1,6 +1,6 @@
-import { ArrowRightIcon, ChevronDownIcon } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { ArrowRightIcon, ChevronDownIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 import {
   Card,
@@ -9,7 +9,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from '@/components/ui/card';
 
 type UpcomingEvents = {
   name: string;
@@ -23,22 +23,22 @@ type UpcomingEvents = {
 
 const upcomingEvents: UpcomingEvents[] = [
   {
-    name: "Niseko - Crypto Ski Week",
-    image: "/images/crypto-ski-week.jpeg",
-    link: "/",
-    date: "31st - 1st",
-    month_year: "March, 2023",
-    location: "Niseko, Japan",
+    name: 'Niseko - Crypto Ski Week',
+    image: '/images/crypto-ski-week.jpeg',
+    link: '/',
+    date: '31st - 1st',
+    month_year: 'March, 2023',
+    location: 'Niseko, Japan',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
   {
-    name: "Niseko - Crypto Ski Week",
-    image: "/images/crypto-ski-week.jpeg",
-    link: "/",
-    date: "31st - 1st",
-    month_year: "March, 2023",
-    location: "Niseko, Japan",
+    name: 'Niseko - Crypto Ski Week',
+    image: '/images/crypto-ski-week.jpeg',
+    link: '/',
+    date: '31st - 1st',
+    month_year: 'March, 2023',
+    location: 'Niseko, Japan',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
@@ -46,45 +46,45 @@ const upcomingEvents: UpcomingEvents[] = [
 
 function UpcomingEvents() {
   return (
-    <section className="w-full flex flex-col gap-y-12 px-12 xl:px-32">
-      <div className="flex flex-col gap-y-4">
-        <h2 className="font-semibold text-4xl">Upcoming Events</h2>
-        <p className="text-xl text-slate-500">
+    <section className='flex w-full flex-col gap-y-12 px-12 xl:px-32'>
+      <div className='flex flex-col gap-y-4'>
+        <h2 className='text-4xl font-semibold'>Upcoming Events</h2>
+        <p className='text-xl text-slate-500'>
           A selection of our upcoming events.
         </p>
       </div>
-      <div className="flex flex-col gap-y-8 ">
+      <div className='flex flex-col gap-y-8 '>
         {upcomingEvents.map((event) => (
-          <div key={event.name} className="flex shadow-lg drop-shadow-sm">
-            <div className="w-96 h-auto relative">
+          <div key={event.name} className='flex shadow-lg drop-shadow-sm'>
+            <div className='relative h-auto w-96'>
               <Image
                 src={event.image}
                 alt={event.name}
                 priority
                 quality={100}
                 fill
-                className="object-cover -z-10 rounded-l-md"
+                className='-z-10 rounded-l-md object-cover'
               />
-              <div className="w-full h-full bg-black opacity-40 absolute -z-10 rounded-l-md" />
-              <div className="flex flex-col p-12 text-white font-poppins w-full h-full justify-center items-center gap-y-2">
-                <span className="font-semibold text-5xl">{event.date}</span>
-                <span className="font-medium">{event.month_year}</span>
+              <div className='absolute -z-10 h-full w-full rounded-l-md bg-black opacity-40' />
+              <div className='flex h-full w-full flex-col items-center justify-center gap-y-2 p-12 font-poppins text-white'>
+                <span className='text-5xl font-semibold'>{event.date}</span>
+                <span className='font-medium'>{event.month_year}</span>
               </div>
             </div>
-            <Card className="p-8 flex-1">
+            <Card className='flex-1 p-8'>
               <CardHeader>
-                <CardTitle className="text-3xl">{event.name}</CardTitle>
-                <CardDescription className="text-slate-400">
+                <CardTitle className='text-3xl'>{event.name}</CardTitle>
+                <CardDescription className='text-slate-400'>
                   {event.location}
                 </CardDescription>
               </CardHeader>
-              <CardContent className="text-slate-400">
+              <CardContent className='text-slate-400'>
                 {event.description}
               </CardContent>
-              <CardFooter className="text-slate-400">
+              <CardFooter className='text-slate-400'>
                 <Link
                   href={event.link}
-                  className="ml-auto flex items-center gap-x-4"
+                  className='ml-auto flex items-center gap-x-4'
                 >
                   <span>View Events Details</span>
                   <ArrowRightIcon />
@@ -93,7 +93,7 @@ function UpcomingEvents() {
             </Card>
           </div>
         ))}
-        <div className="flex items-center justify-center font-medium text-2xl text-slate-500 gap-x-4 cursor-pointer w-fit mx-auto">
+        <div className='mx-auto flex w-fit cursor-pointer items-center justify-center gap-x-4 text-2xl font-medium text-slate-500'>
           <span>See All</span>
           <ChevronDownIcon />
         </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client';
+import unitLogoBlack from '@public/images/unit-logo-black.svg';
+import unitLogoWhite from '@public/images/unit-logo-white.svg';
+import { useWindowSize } from '@uidotdev/usehooks';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { CgMenuRightAlt } from 'react-icons/cg';
 
-import unitLogoBlack from "@public/images/unit-logo-black.svg";
-import unitLogoWhite from "@public/images/unit-logo-white.svg";
-import { useWindowSize } from "@uidotdev/usehooks";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { cn } from '@/lib/utils';
 
-import { cn } from "@/lib/utils";
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
 
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -21,39 +23,60 @@ function Navbar() {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", checkScroll);
+    window.addEventListener('scroll', checkScroll);
     return () => {
-      window.removeEventListener("scroll", checkScroll);
+      window.removeEventListener('scroll', checkScroll);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [height]);
 
   return (
-    <div
-      className={cn(
-        "w-full fixed top-0 z-50 left-0",
-        isScrolled ? "bg-white shadow-sm" : "bg-transparent",
-      )}
-    >
+    <Sheet>
       <div
         className={cn(
-          "w-full px-12 xl:px-32 flex items-center gap-x-8 py-6 left-0 right-0 font-medium font-poppins transition-all",
-          isScrolled ? "text-unit-black" : "text-white",
+          'fixed left-0 top-0 z-50 w-full',
+          isScrolled ? 'bg-white shadow-sm' : 'bg-transparent',
         )}
       >
-        <Image
-          src={isScrolled ? unitLogoBlack : unitLogoWhite}
-          alt="Unit Network"
-          width={48}
-          height={48}
-          priority
-          className="mr-auto"
-        />
-        <Link href="#">Unit Network</Link>
-        <Link href="#">Events</Link>
-        <Link href="#">FAQ</Link>
+        <div
+          className={cn(
+            'left-0 right-0 flex w-full items-center gap-x-8 px-8 py-6 font-poppins font-medium transition-all sm:px-16 md:px-24 lg:px-32',
+            isScrolled ? 'text-unit-black' : 'text-white',
+          )}
+        >
+          <div className='relative mr-auto size-8 lg:size-12'>
+            <Image
+              src={isScrolled ? unitLogoBlack : unitLogoWhite}
+              alt='Unit Network'
+              fill
+              priority
+              quality={100}
+            />
+          </div>
+          <div className='hidden items-center gap-x-8 md:flex'>
+            <Link href='#'>Unit Network</Link>
+            <Link href='#'>Events</Link>
+            <Link href='#'>FAQ</Link>
+          </div>
+          <SheetTrigger>
+            <CgMenuRightAlt className='block size-8 md:hidden' />
+          </SheetTrigger>
+        </div>
       </div>
-    </div>
+      <SheetContent className='w-full'>
+        <ul className='flex flex-col items-end justify-end gap-y-6 font-poppins text-xl font-semibold'>
+          <li>
+            <Link href='#'>Unit Network</Link>
+          </li>
+          <li>
+            <Link href='#'>Events</Link>
+          </li>
+          <li>
+            <Link href='#'>FAQ</Link>
+          </li>
+        </ul>
+      </SheetContent>
+    </Sheet>
   );
 }
 
