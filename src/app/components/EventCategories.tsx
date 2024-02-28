8f9ab0f764ccf61b2184e05d2b708696e9a7657b
shrall-unit <marshall.kurniawan@unitcode.org>
2024-02-28 14:19:51 +0700

    fix: event categories section wasn't scrollable

--- src/app/components/EventCategories.tsx
+++ src/app/components/EventCategories.tsx
@@ -5,7 +5,13 @@ import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
 
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 type Events = {
   name: string;
@@ -58,55 +64,70 @@ const events: Events[] = [
 
 function EventCategories() {
   return (
-    <section className='flex w-full flex-col gap-y-12 pl-12 xl:pl-32'>
-      <div className='flex flex-col gap-y-4 pr-12'>
+    <section className='flex w-full flex-col gap-y-12'>
+      <div className='flex flex-col gap-y-4 px-12 xl:px-32'>
         <h2 className='text-3xl font-semibold md:text-4xl'>Event Categories</h2>
         <p className='text-lg text-slate-500 md:text-xl'>
           Discover a curated selection of events designed to captivate and
           engage every crypto enthusiast.
         </p>
       </div>
-      <ScrollArea className='w-full whitespace-nowrap'>
-        <div className='flex w-max gap-x-6'>
+      <Carousel
+        opts={{
+          align: 'start',
+          skipSnaps: true,
+        }}
+        className='group/carousel w-full'
+      >
+        <CarouselContent className='mx-12 xl:mx-32'>
           {events.map((event, index) => (
-            <div
+            <CarouselItem
               key={event.name}
-              className={cn(
-                'relative h-80 w-72 overflow-hidden rounded-md text-white',
-                events.length - 1 === index && 'mr-12 md:mr-32',
-              )}
+              className='flex w-max gap-x-6 transition-all duration-500 hover:scale-105 hover:shadow-sm'
             >
-              <Image
-                src={event.image}
-                alt={event.name}
-                priority={true}
-                quality={100}
-                fill
-                className='-z-10 object-cover'
-              />
-              <div className='flex h-full flex-col items-center justify-center gap-y-4 break-words p-8 text-center'>
+              <div
+                className={cn(
+                  'relative h-96 w-72 overflow-hidden rounded-2xl text-white',
+                  events.length - 1 === index && 'mr-12 md:mr-32',
+                )}
+              >
                 <Image
-                  src={unitLogoWhite}
+                  src={event.image}
                   alt={event.name}
-                  width={32}
-                  height={32}
                   priority={true}
+                  quality={100}
+                  fill
+                  className='-z-10 object-cover'
                 />
-                <div className='absolute -z-10 h-full w-full bg-black opacity-40'></div>
-                <h4 className='mt-auto whitespace-break-spaces text-3xl font-semibold'>
-                  {event.name}
-                </h4>
-                <Link href={event.link}>
-                  <div className='flex items-center gap-x-1 font-medium'>
-                    Explore <MoveUpRightIcon />
+                <div className='flex h-full flex-col items-center justify-center gap-y-4 break-words p-8'>
+                  <Image
+                    src={unitLogoWhite}
+                    alt={event.name}
+                    width={32}
+                    height={32}
+                    priority={true}
+                    className='self-start'
+                  />
+                  <div className='absolute -z-10 h-full w-full bg-black opacity-40'></div>
+                  <div className='mt-auto flex flex-col'>
+                    <h4 className='whitespace-break-spaces text-3xl font-semibold'>
+                      {event.name}
+                    </h4>
+                    <p>Lorem ipsum</p>
                   </div>
-                </Link>
+                  <Link href={event.link} className='w-full'>
+                    <div className='flex items-center gap-x-1 font-medium'>
+                      Explore <MoveUpRightIcon />
+                    </div>
+                  </Link>
+                </div>
               </div>
-            </div>
+            </CarouselItem>
           ))}
-        </div>
-        <ScrollBar orientation='horizontal' />
-      </ScrollArea>
+        </CarouselContent>
+        <CarouselPrevious className='hidden !opacity-0 transition-all duration-500 group-hover/carousel:scale-110 group-hover/carousel:!opacity-100 md:flex' />
+        <CarouselNext className='hidden !opacity-0 transition-all duration-500 group-hover/carousel:scale-110 group-hover/carousel:!opacity-100 md:flex' />
+      </Carousel>
     </section>
   );
 }
