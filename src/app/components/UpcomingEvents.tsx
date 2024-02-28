1b28ab5208a346e5f682038861f0fc9a16175499
shrall-unit <marshall.kurniawan@unitcode.org>
2024-02-28 12:02:59 +0700

    feat: add mobile upcoming events section

--- src/app/components/UpcomingEvents.tsx
+++ src/app/components/UpcomingEvents.tsx
@@ -46,54 +46,87 @@ const upcomingEvents: UpcomingEvents[] = [
 
 function UpcomingEvents() {
   return (
-    <section className='flex w-full flex-col gap-y-12 px-12 xl:px-32'>
-      <div className='flex flex-col gap-y-4'>
-        <h2 className='text-4xl font-semibold'>Upcoming Events</h2>
-        <p className='text-xl text-slate-500'>
+    <section className='flex w-full flex-col gap-y-12 lg:px-12 xl:px-32'>
+      <div className='flex flex-col gap-y-4 px-12 lg:px-0'>
+        <h2 className='text-3xl font-semibold md:text-4xl'>Upcoming Events</h2>
+        <p className='text-lg text-slate-500 md:text-xl'>
           A selection of our upcoming events.
         </p>
       </div>
       <div className='flex flex-col gap-y-8 '>
         {upcomingEvents.map((event) => (
-          <div key={event.name} className='flex shadow-lg drop-shadow-sm'>
-            <div className='relative h-auto w-96'>
-              <Image
-                src={event.image}
-                alt={event.name}
-                priority
-                quality={100}
-                fill
-                className='-z-10 rounded-l-md object-cover'
-              />
-              <div className='absolute -z-10 h-full w-full rounded-l-md bg-black opacity-40' />
-              <div className='flex h-full w-full flex-col items-center justify-center gap-y-2 p-12 font-poppins text-white'>
-                <span className='text-5xl font-semibold'>{event.date}</span>
-                <span className='font-medium'>{event.month_year}</span>
+          <div key={event.name}>
+            <div className='hidden shadow-lg drop-shadow-sm lg:flex'>
+              <div className='relative h-auto w-96'>
+                <Image
+                  src={event.image}
+                  alt={event.name}
+                  priority
+                  quality={100}
+                  fill
+                  className='-z-10 rounded-l-md object-cover'
+                />
+                <div className='absolute -z-10 h-full w-full rounded-l-md bg-black opacity-40' />
+                <div className='flex h-full w-full flex-col items-center justify-center gap-y-2 p-12 font-poppins text-white'>
+                  <span className='text-5xl font-semibold'>{event.date}</span>
+                  <span className='font-medium'>{event.month_year}</span>
+                </div>
               </div>
+              <Card className='flex-1 p-8'>
+                <CardHeader>
+                  <CardTitle className='text-3xl'>{event.name}</CardTitle>
+                  <CardDescription className='text-slate-400'>
+                    {event.location}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className='text-slate-400'>
+                  {event.description}
+                </CardContent>
+                <CardFooter className='text-slate-400'>
+                  <Link
+                    href={event.link}
+                    className='ml-auto flex items-center gap-x-4'
+                  >
+                    <span>View Events Details</span>
+                    <ArrowRightIcon />
+                  </Link>
+                </CardFooter>
+              </Card>
             </div>
-            <Card className='flex-1 p-8'>
-              <CardHeader>
-                <CardTitle className='text-3xl'>{event.name}</CardTitle>
-                <CardDescription className='text-slate-400'>
-                  {event.location}
-                </CardDescription>
-              </CardHeader>
-              <CardContent className='text-slate-400'>
+            <div className='flex flex-col gap-y-6 lg:hidden'>
+              <div className='relative h-auto w-full'>
+                <Image
+                  src={event.image}
+                  alt={event.name}
+                  priority
+                  quality={100}
+                  fill
+                  className='-z-10 object-cover object-top'
+                />
+                <div className='absolute -z-10 h-full w-full rounded-l-md bg-black opacity-40' />
+                <div className='flex h-full w-full flex-col items-center justify-center gap-y-2 p-12 font-poppins text-white'>
+                  <span className='text-5xl font-semibold'>{event.date}</span>
+                  <span className='font-medium'>{event.month_year}</span>
+                </div>
+              </div>
+              <div className='flex flex-col gap-y-2 px-12'>
+                <h3 className='text-2xl font-semibold'>{event.name}</h3>
+                <p className='text-sm text-slate-400'>{event.location}</p>
+              </div>
+              <p className='px-12 text-sm text-slate-400'>
                 {event.description}
-              </CardContent>
-              <CardFooter className='text-slate-400'>
-                <Link
-                  href={event.link}
-                  className='ml-auto flex items-center gap-x-4'
-                >
-                  <span>View Events Details</span>
-                  <ArrowRightIcon />
-                </Link>
-              </CardFooter>
-            </Card>
+              </p>
+              <Link
+                href={event.link}
+                className='flex items-center justify-center gap-x-4 px-12 text-sm text-slate-400'
+              >
+                <span>View Events Details</span>
+                <ArrowRightIcon />
+              </Link>
+            </div>
           </div>
         ))}
-        <div className='mx-auto flex w-fit cursor-pointer items-center justify-center gap-x-4 text-2xl font-medium text-slate-500'>
+        <div className='mx-auto flex w-full cursor-pointer items-center justify-center gap-x-4 bg-neutral-100 py-2 text-sm font-medium text-slate-400 lg:w-fit lg:bg-transparent lg:text-2xl lg:text-slate-500'>
           <span>See All</span>
           <ChevronDownIcon />
         </div>
