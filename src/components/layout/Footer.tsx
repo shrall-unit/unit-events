95790d68354795be8b07adf56da8d15c73bdcc7d
shrall-unit <marshall.kurniawan@unitcode.org>
2024-03-15 15:10:19 +0700

    feat: update footer social links

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ import {
   FaFacebookF,
   FaLinkedinIn,
   FaTelegram,
-  FaTwitter,
+  FaXTwitter,
   FaYoutube,
 } from 'react-icons/fa6';
 
@@ -14,19 +14,19 @@ function Footer() {
   return (
     <div className='flex w-full flex-col items-center justify-center gap-y-8 bg-unit-grey-10 py-32 text-unit-grey-40'>
       <div className='flex items-center justify-center gap-x-4'>
-        <Link href='#'>
-          <FaTwitter className='size-6' />
+        <Link href='https://twitter.com/theunitnetwork'>
+          <FaXTwitter className='size-6' />
         </Link>
-        <Link href='#'>
+        <Link href='https://www.facebook.com/theunitnetwork'>
           <FaFacebookF className='size-6' />
         </Link>
-        <Link href='#'>
+        <Link href='https://www.linkedin.com/company/theunitnetwork/'>
           <FaLinkedinIn className='size-6' />
         </Link>
-        <Link href='#'>
+        <Link href='https://www.youtube.com/c/UnitGlobal'>
           <FaYoutube className='size-6' />
         </Link>
-        <Link href='#'>
+        <Link href='https://t.me/unitfoundation'>
           <FaTelegram className='size-6' />
         </Link>
       </div>
